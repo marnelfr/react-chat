8269b8e66e8e2b2ba3199c75a02fcadcfc054835
Marnel Gnacadja <gmlginolias@gmail.com>
2023-04-04 21:24:16 +0100

    Working on chat

--- src/features/messenger/thunks/chat-thunk.ts
+++ src/features/messenger/thunks/chat-thunk.ts
@@ -2,25 +2,32 @@ import { AnyAction, ThunkAction } from "@reduxjs/toolkit";
 import { RootState } from "../../../app/store";
 import { chatActions, Message } from "../slices/chat";
 import { loadActions } from "../slices/loading";
+import apiClient from "../../../helpers/fetch";
 
 export const sendMessage = (
-  text: string
+  text: string,
+  chat?: string
 ): ThunkAction<void, RootState, unknown, AnyAction> => {
   return async (dispatch) => {
     dispatch(chatActions.send(text));
     dispatch(loadActions.set(true));
 
+    const responseData = await apiClient.authPost(
+      chat ? "message" : "new/chat",
+      { text, chat }
+    );
+
     // todo: update this api to create a new chat and save the question about it and then return the chat and the response
-    const response = await fetch("http://localhost:8080/api/message", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ text }),
-    });
-
-    const responseData = await response.json();
-
+    // const response = await fetch("http://localhost:8080/api/new/chat", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ text }),
+    // });
+    //
+    // const responseData = await response.json();
+    //
     dispatch(loadActions.set(false));
 
     const data: Message = {
