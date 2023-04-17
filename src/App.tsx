5270787505d0922c4e188ed092a1e6169f3376c5
Marnel Gnacadja <gmlginolias@gmail.com>
2023-04-17 14:47:19 +0100

    Using react-router in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import LoginPage from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
+import RootLayout from "./pages/Layout/Root";
 
 const App = () => {
-  return <LoginPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route path="login" element={<LoginPage />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
